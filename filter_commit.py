4b8d89fc6b4c89bd13c2dbaf7623d81c99c20e1e
Thong Hoang <hvdthong@Thongs-MacBook-Pro.local>
2017-10-25 16:09:17 +0800

    update filtering data

--- filter_commit.py
+++ filter_commit.py
@@ -18,6 +18,25 @@ def filter_number_code_hunk(commits, num_hunk):
         if max(cnt_hunk) <= num_hunk:
             commit_id.append(c["id"])
     return commit_id
-    # cnt = [c for c in count if c <= 8]
-    # print len(cnt)
-    # histogram(data=count)
+
+
+def filter_loc_hunk(commits, num_loc):
+    count = list()
+    for c in commits:
+        files = c["code"]
+        cnt_loc_hunk = list()
+        for hunk in files:
+            first = hunk
+            # max_hunk = max(first["removed"].keys() + first["added"].keys())
+            # if max_hunk <= 8:
+            removed_code, added_code = hunk["removed"], hunk["added"]
+            if len(hunk["removed"].keys()) > 0:
+                max_removed_code = max([len(removed_code[k]) for k in hunk["removed"].keys()])
+            else:
+                max_removed_code = 0
+            if len(first["added"].keys()) > 0:
+                max_added_code = max([len(added_code[k]) for k in first["added"].keys()])
+            else:
+                max_added_code = 0
+            # max_added_code = max([len(added_code[k]) for k in first["added"].keys()])
+            count.append(max(max_removed_code, max_added_code))

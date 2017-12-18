60c5f4eeacf55875db7947f0492704a12cc84d5f
hvdthong <hvdthong@gmail.com>
2017-12-18 18:23:37 +0800

    adding train

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ from data_helpers import dictionary, mapping_commit_msg, mapping_commit_code, lo
 from baselines import get_items
 import numpy as np
 from cnn_model import CNN_model
+import os
+import time
+import datetime
 
 ################################################################################################
 tf = model_parameters()
@@ -24,6 +27,8 @@ labels = load_label_commits(commits=filter_commits)
 print pad_msg.shape, pad_code.shape, labels.shape
 ################################################################################################
 kf = KFold(n_splits=FLAGS.folds, random_state=FLAGS.seed)
+cntfold = 0
+timestamp = str(int(time.time()))
 for train_index, test_index in kf.split(filter_commits):
     X_train_msg, X_test_msg = np.array(get_items(items=pad_msg, indexes=train_index)), \
                               np.array(get_items(items=pad_msg, indexes=test_index))
@@ -55,4 +60,58 @@ for train_index, test_index in kf.split(filter_commits):
             optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate)
             grads_and_vars = optimizer.compute_gradients(cnn.loss)
             train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+            out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", "fold_" + str(cntfold) + "_" + timestamp))
+            print("Writing to {}\n".format(out_dir))
+
+            # Summaries for loss and accuracy
+            loss_summary = tf.summary.scalar("loss", cnn.loss)
+
+            # Train Summaries
+            train_summary_op = tf.summary.merge([loss_summary])
+            train_summary_dir = os.path.join(out_dir, "summaries", "train")
+            train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+            # Dev Summaries
+            dev_summary_op = tf.summary.merge([loss_summary])
+            dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+            dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
+
+            # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
+            checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+            checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+            if not os.path.exists(checkpoint_dir):
+                os.makedirs(checkpoint_dir)
+            saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
+
+            # Initialize all variables
+            sess.run(tf.global_variables_initializer())
+
+
+            def train_step(left_text, left_add_code, left_remove_code, left_aux_ftr,
+                           right_text, right_addedcode, right_remove_code, right_aux_ftr):
+                """
+                A training step
+                """
+                feed_dict = {
+                    cnn.input_text_left: left_text,
+                    cnn.input_addedcode_left: left_add_code,
+                    cnn.input_removedcode_left: left_remove_code,
+                    cnn.input_auxftr_left: left_aux_ftr,
+                    cnn.input_text_right: right_text,
+                    cnn.input_addedcode_right: right_addedcode,
+                    cnn.input_removedcode_right: right_remove_code,
+                    cnn.input_auxftr_right: right_aux_ftr,
+                    cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
+                }
+
+                _, step, summaries, loss = sess.run(
+                    [train_op, global_step, train_summary_op, cnn.loss],
+                    feed_dict)
+
+                time_str = datetime.datetime.now().isoformat()
+                print("{}: step {}, loss {:g}".format(time_str, step, loss))
+                train_summary_writer.add_summary(summaries, step)
+
+
     exit()

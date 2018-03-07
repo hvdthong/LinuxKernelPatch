c723c56b49e42158752ee83b585999a967b4c753
hvdthong <hvdthong@github.com>
2018-03-07 10:54:00 +0800

    adding statistical test

--- statistical_test.py
+++ statistical_test.py
@@ -1,2 +1,15 @@
 if __name__ == "__main__":
-    print "hello"
+    root_file = "./statistical_test/fold_0_1518703738_model-46656.txt"
+    compare_files = list()
+    compare_files.append("./statistical_test/code_dt.txt")
+    compare_files.append("./statistical_test/code_lr.txt")
+    compare_files.append("./statistical_test/code_svm.txt")
+    compare_files.append("./statistical_test/msg_code_dt.txt")
+    compare_files.append("./statistical_test/msg_code_lr.txt")
+    compare_files.append("./statistical_test/msg_code_svm.txt")
+    compare_files.append("./statistical_test/msg_dt.txt")
+    compare_files.append("./statistical_test/msg_lr.txt")
+    compare_files.append("./statistical_test/msg_svm.txt")
+
+    print root_file
+    print len(compare_files)

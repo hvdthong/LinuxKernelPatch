b7e95cb50f6c87c6d4b1b0d3702f66b95713ccad
hvdthong <hvdthong@github.com>
2018-03-19 15:59:37 +0800

    adding lstm

--- lstm_model.py
+++ lstm_model.py
@@ -1,1 +1,6 @@
 class lstm_model(object):
+    # ==================================================
+    # ==================================================
+    def __init__(self, max_msg_length, max_code_length, max_code_line, max_code_hunk, vocab_size_text,
+                 vocab_size_code, embedding_size_text, filter_sizes, num_filters, l2_reg_lambda, num_classes):
+        print "hello"
